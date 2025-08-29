fa4eff48cd2db1aa16a05c17976cbe4b4592aac2
Han-nu-ri <nrhan0623@gmail.com>
2025-08-29 11:07:27 +0900

    mission1-commit1

---
+++ mission1/attendance.py
@@ -0,0 +1,143 @@
+from typing import Dict
+
+
+class Player:
+    def __init__(self, name, back_number):
+        self.name = name
+        self.back_number = back_number
+        self.points = 0
+        self.grade = None
+        self.attendance_counts: Dict[str, int] = {'monday': 0, 'tuesday': 0, 'wednesday': 0, 'thursday': 0, 'friday': 0,
+                                                  'saturday': 0, 'sunday': 0}
+
+
+class AttendanceCalculator:
+    def __init__(self):
+        self.attendance_records = []
+        self.name_and_back_number = {}
+        self.max_back_number = 0
+        self.players: Dict[int, Player] = {}
+
+    def add_basic_point(self, attendance_day, back_number):
+        wednesday_point = 3
+        weekend_point = 2
+        basic_point = 1
+        if attendance_day == "wednesday":
+            self.players[back_number].points += wednesday_point
+        elif attendance_day == "saturday" or attendance_day == "sunday":
+            self.players[back_number].points += weekend_point
+        else:
+            self.players[back_number].points += basic_point
+
+    def get_back_number(self, name):
+        if name not in self.name_and_back_number:
+            self.max_back_number += 1
+            self.name_and_back_number[name] = self.max_back_number
+            self.players[self.max_back_number] = Player(name, self.max_back_number)
+        return self.name_and_back_number[name]
+
+    def calculate_attendance_points(self):
+        try:
+            self.read_attendance_records()
+            self.calculate_attendance_days_and_points()
+            self.add_bonus()
+            self.analyze_grades()
+            self.print_grades()
+            self.print_removed_players()
+        except FileNotFoundError:
+            print("파일을 찾을 수 없습니다.")
+        except Exception as exception:
+            print(f"{exception}")
+
+    def print_removed_players(self):
+        print("\nRemoved player")
+        print("==============")
+        for back_number in range(1, self.max_back_number + 1):
+            if self.is_removed(back_number):
+                print(self.players[back_number].name)
+
+    def is_removed(self, back_number):
+        return self.players[back_number].grade == "NORMAL" and (
+                self.players[back_number].attendance_counts['saturday']
+                + self.players[back_number].attendance_counts['sunday']) == 0 and \
+            self.players[back_number].attendance_counts['wednesday'] == 0
+
+    def analyze_grades(self):
+        gold_grade_threshold = 50
+        silver_grade_threshold = 30
+        for back_number in range(1, self.max_back_number + 1):
+            if self.players[back_number].points >= gold_grade_threshold:
+                self.players[back_number].grade = "GOLD"
+            elif self.players[back_number].points >= silver_grade_threshold:
+                self.players[back_number].grade = "SILVER"
+            else:
+                self.players[back_number].grade = "NORMAL"
+
+    def add_bonus(self):
+        bonus_threshold = 10
+        bonus_point = 10
+        for back_number in range(1, self.max_back_number + 1):
+            if self.players[back_number].attendance_counts['wednesday'] >= bonus_threshold:
+                self.players[back_number].points += bonus_point
+            if self.players[back_number].attendance_counts['saturday'] + self.players[back_number].attendance_counts[
+                'sunday'] >= bonus_threshold:
+                self.players[back_number].points += bonus_point
+
+    def print_grades(self):
+        for back_number in range(1, self.max_back_number + 1):
+            print(f"NAME : {self.players[back_number].name}, POINT : {self.players[back_number].points}, GRADE : ",
+                  end="")
+            print(self.players[back_number].grade)
+
+    def read_attendance_records(self):
+        attendance_records_file = "attendance_weekday_500.txt"
+        with open(attendance_records_file, encoding='utf-8') as f:
+            for _ in range(500):
+                line = f.readline()
+                if not line:
+                    break
+                self.attendance_records.append(line)
+
+    def calculate_attendance_days_and_points(self):
+        for each_line in self.attendance_records:
+            name_and_day = each_line.strip().split()
+            if len(name_and_day) == 2:
+                name = name_and_day[0]
+                attendance_day = name_and_day[1]
+                back_number = self.get_back_number(name)
+                self.add_basic_point(attendance_day, back_number)
+                self.players[back_number].attendance_counts[attendance_day] += 1
+            else:
+                raise Exception(
+                    f"Invalid Inputs. Each line must contain two str separated by space, But input is {each_line}")
+
+
+if __name__ == "__main__":
+    AttendanceCalculator().calculate_attendance_points()
+#     WHOLE_TEST = """NAME : Umar, POINT : 48, GRADE : SILVER
+# NAME : Daisy, POINT : 45, GRADE : SILVER
+# NAME : Alice, POINT : 61, GRADE : GOLD
+# NAME : Xena, POINT : 91, GRADE : GOLD
+# NAME : Ian, POINT : 23, GRADE : NORMAL
+# NAME : Hannah, POINT : 127, GRADE : GOLD
+# NAME : Ethan, POINT : 44, GRADE : SILVER
+# NAME : Vera, POINT : 22, GRADE : NORMAL
+# NAME : Rachel, POINT : 54, GRADE : GOLD
+# NAME : Charlie, POINT : 58, GRADE : GOLD
+# NAME : Steve, POINT : 38, GRADE : SILVER
+# NAME : Nina, POINT : 79, GRADE : GOLD
+# NAME : Bob, POINT : 8, GRADE : NORMAL
+# NAME : George, POINT : 42, GRADE : SILVER
+# NAME : Quinn, POINT : 6, GRADE : NORMAL
+# NAME : Tina, POINT : 24, GRADE : NORMAL
+# NAME : Will, POINT : 36, GRADE : SILVER
+# NAME : Oscar, POINT : 13, GRADE : NORMAL
+# NAME : Zane, POINT : 1, GRADE : NORMAL
+#
+# Removed player
+# ==============
+# Bob
+# Zane"""
+#
+#     captured = capsys.readouterr()
+#     assert captured.out == WHOLE_TEST
